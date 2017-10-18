b21659b46e223a923dd2d2c5d459aec234a40d12
JaspreetR <jaspreetsrandhawa@hotmail.com>
2017-10-18 12:19:14 +0100

    add new project

---
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/MainActivity.java
@@ -0,0 +1,13 @@
+package g1736229.elderlyui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
