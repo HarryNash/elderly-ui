2f7c04b43e517a38f0b4dfad812be236574ee709
unknown <evgenia.kaneva14@imperial.ac.uk>
2017-10-19 17:12:09 +0100

    attempt1 to inject random contacts in contact app

---
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/InitialiseSampleContactData.java
@@ -0,0 +1,62 @@
+package g1736229.elderlyui;
+
+import android.content.ContentProviderOperation;
+import android.content.Context;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Evche on 19/10/2017.
+ */
+
+public class InitialiseSampleContactData {
+    public void addContactData(Context context, ContactInfo contactInfo){
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        if (contactInfo.getName() != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                            contactInfo.getName()).build());
+        }
+
+        // Mobile Number
+        if (contactInfo.getPhoneNumber() != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, contactInfo.getPhoneNumber())
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
+        }
+
+        // Email
+        if (contactInfo.getEmail() != null) {
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                    .withValue(ContactsContract.Data.MIMETYPE,
+                            ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.DATA, contactInfo.getEmail())
+                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE,
+                            ContactsContract.CommonDataKinds.Email.TYPE_WORK).build());
+        }
+        //TODO : initialise a picture too;
+
+        // Asking the Contact provider to create a new contact
+        try {
+            context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- ElderlyUI/app/src/main/java/g1736229/elderlyui/MainActivity.java
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/MainActivity.java
@@ -1,5 +1,6 @@
 package g1736229.elderlyui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,11 +8,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "g17361229.elderlyui.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //private function - fakes the contacts
+        initialiseSampleContactData();
+
+        //private function - pulls the fake contacts
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
                 sendContactInfo(v);
             }
         });
+    }
+
+    private void initialiseSampleContactData() {
+        ArrayList<ContactInfo> contactInfoArray = new ArrayList<ContactInfo>();
+
+        contactInfoArray.add(new ContactInfo("Aleksandar", "02079460498", "aleks@abc.com"));
+        contactInfoArray.add(new ContactInfo("Maria", "02079461235", "maria@abc.com"));
+        contactInfoArray.add(new ContactInfo("Tatiana", "02079423456", "tatiana@abc.com"));
+        contactInfoArray.add(new ContactInfo("Borislav", "02079456789", "borislav@abc.com"));
+        contactInfoArray.add(new ContactInfo("Silvana", "02079402364", "silvana@abc.com"));
+        contactInfoArray.add(new ContactInfo("Krasimira", "02079445623", "krasimira@abc.com"));
+        contactInfoArray.add(new ContactInfo("Richard", "02079411447", "richard@abc.com"));
+        contactInfoArray.add(new ContactInfo("Tom", "02079425896", "tom@abc.com"));
+
+        InitialiseSampleContactData initialiseSampleContactData =
+                new InitialiseSampleContactData();
+        for (ContactInfo contact : contactInfoArray){
+            initialiseSampleContactData.addContactData(this, contact);
+        }
     }
 
     public void sendContactInfo(View view) {
