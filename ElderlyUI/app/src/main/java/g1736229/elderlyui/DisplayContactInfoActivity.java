d28b49e653d19b61e34158b3e2f731784163eb56
JaspreetR <jaspreetsrandhawa@hotmail.com>
2017-11-15 15:46:15 +0000

    add text message button with corresponding functionality. Add voice to speech for text messaging in app

--- ElderlyUI/app/src/main/java/g1736229/elderlyui/DisplayContactInfoActivity.java
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/DisplayContactInfoActivity.java
@@ -1,15 +1,27 @@
 package g1736229.elderlyui;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class DisplayContactInfoActivity extends AppCompatActivity {
     String componentSize;
-    String theNumber = "";
+    String phoneNumber = "";
+
+    // User option for voice to text messaging
+    boolean voiceOnly = true;
+
+    // Code for voice input
+    final int REQ_CODE_SPEECH_INPUT = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,7 @@ public class DisplayContactInfoActivity extends AppCompatActivity {
         nameText.setTextSize(textSize);
 
         TextView phoneText = (TextView) findViewById(R.id.textView2);
-        theNumber = convertNull(contactInfo.getPhoneNumber());
+        phoneNumber = convertNull(contactInfo.getPhoneNumber());
         phoneText.setText(convertNull(contactInfo.getPhoneNumber()));
         phoneText.setTextSize(textSize);
 
@@ -37,11 +49,21 @@ public class DisplayContactInfoActivity extends AppCompatActivity {
         emailText.setText(convertNull(contactInfo.getEmail()));
         emailText.setTextSize(textSize);
 
-        ComponentResizing.resizeButton(componentSize, findViewById(R.id.button6), getResources());
+        ComponentResizing.resizeButton(componentSize, findViewById(R.id.callButton), getResources());
+        ComponentResizing.resizeButton(componentSize, findViewById(R.id.messageButton), getResources());
     }
 
     public void makeCall(View view) {
-        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", theNumber, null)));
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
+    public void makeText(View view) {
+        if (voiceOnly) {
+            startVoiceInput();
+        } else {
+            Intent intent = createTextMessageIntent(phoneNumber);
+            startActivity(intent);
+        }
     }
 
     private String convertNull(String string) {
@@ -50,4 +72,44 @@ public class DisplayContactInfoActivity extends AppCompatActivity {
         }
         return string;
     }
+
+    private Intent createTextMessageIntent(String phoneNumber)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("sms:" + phoneNumber));
+        return intent;
+    }
+
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Please say your message");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast toast = Toast.makeText(this, "Voice input not available", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Intent intent = createTextMessageIntent(phoneNumber);
+                    intent.putExtra("sms_body", result.get(0));
+                    startActivity(intent);
+                }
+                break;
+            }
+
+        }
+    }
 }
+
