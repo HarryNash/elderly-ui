87d77562edc8728bd9ff49154e17fff1d764ba41
Jinsung Ha <jsung5381@naver.com>
2017-11-29 22:47:23 +0000

    Add camera feature to the feature selection screen.

--- ElderlyUI/app/src/main/java/g1736229/elderlyui/FeatureSelectionActivity.java
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/FeatureSelectionActivity.java
@@ -1,9 +1,14 @@
 package g1736229.elderlyui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class FeatureSelectionActivity extends AppCompatActivity {
     String componentSize;
@@ -19,6 +24,7 @@ public class FeatureSelectionActivity extends AppCompatActivity {
         componentSize = intent.getStringExtra(ImpairmentDetectionActivity.EXTRA_COMPONENT_SIZE);
         headingStyle = intent.getStringExtra(ImpairmentDetectionActivity.HEADING_STYLE);
         ComponentResizing.resizeButton(headingStyle, componentSize, findViewById(R.id.gallery), getResources());
+        ComponentResizing.resizeButton(headingStyle, componentSize, findViewById(R.id.camera), getResources());
         ComponentResizing.resizeButton(headingStyle, componentSize, findViewById(R.id.contacts), getResources());
     }
 
@@ -26,6 +32,28 @@ public class FeatureSelectionActivity extends AppCompatActivity {
         Intent cameraIntent = new Intent(Intent.ACTION_VIEW,
                 android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(cameraIntent, GALLERY_CODE);
+    }
+
+    public boolean checkPermissionForCamera(){
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void requestPermissionForCamera(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+            Toast.makeText(this, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, 1234);
+        }
+    }
+
+    public void openCameraActivity(View v) {
+        if (!checkPermissionForCamera())
+            requestPermissionForCamera();
+
+        Intent cameraIntent =
+                new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 1234);
     }
 
     public void openContactsActivity(View v) {
