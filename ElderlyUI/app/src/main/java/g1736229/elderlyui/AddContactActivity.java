c51b8f14f574ddb58c087e7f230f6b33d6ad126f
Rishiraj Bheenick <rnb14@ic.ac.uk>
2017-11-30 17:45:24 +0000

    can add name number and email, photo not yet working

--- ElderlyUI/app/src/main/java/g1736229/elderlyui/AddContactActivity.java
+++ ElderlyUI/app/src/main/java/g1736229/elderlyui/AddContactActivity.java
@@ -1,8 +1,19 @@
 package g1736229.elderlyui;
 
+import android.content.ContentProviderOperation;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.beardedhen.androidbootstrap.BootstrapLabel;
+
+import java.util.ArrayList;
 
 import static g1736229.elderlyui.ContactSelectionActivity.EXTRA_COMPONENT_SIZE;
 
@@ -21,6 +32,68 @@ public class AddContactActivity extends AppCompatActivity {
         headingStyle = intent.getStringExtra(ImpairmentDetectionActivity.HEADING_STYLE);
 
         ComponentResizing.resizeButton(headingStyle, componentSize, findViewById(R.id.confirmadd), getResources());
+
+        View.OnClickListener addContactListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText contactName = (EditText) findViewById(R.id.contactname);
+
+                EditText contactPhone = (EditText) findViewById(R.id.contactnumber);
+
+                EditText contactEmail = (EditText) findViewById(R.id.contactemail);
+
+
+
+                ArrayList<ContentProviderOperation> ops =
+                        new ArrayList<ContentProviderOperation>();
+
+                int rawContactID = ops.size();
+
+
+                // insert a new raw contact in the table ContactsContract.RawContacts
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                        .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                        .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                        .build());
+
+
+                // insert display name in the table ContactsContract.Data
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, contactName.getText().toString())
+                        .build());
+
+                //insert Mobile Number in the table ContactsContract.Data
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, contactPhone.getText().toString())
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                        .build());
+
+                ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactID)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Email.ADDRESS, contactEmail.getText().toString())
+                        .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                        .build());
+
+                try{
+                    getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+                    Toast.makeText(getBaseContext(), "Contact successfully added", Toast.LENGTH_SHORT).show();
+                }catch (RemoteException | OperationApplicationException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        };
+
+        BootstrapLabel addContactButton = (BootstrapLabel) findViewById(R.id.confirmadd);
+
+        addContactButton.setOnClickListener(addContactListener);
 
     }
 }
